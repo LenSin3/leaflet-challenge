db7f02c5967803123c6562d962bb8e77cf328a9a
LenSin3 <lenpk3@yahoo.co.uk>
2020-12-19 00:14:42 -0800

    Get geojason data

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,4 @@
+/*
 // Creating map object
 var myMap = L.map("mapid", {
     center: [34.0522, -118.2437],
@@ -13,15 +14,36 @@ var myMap = L.map("mapid", {
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
-/*
+  */
+
 
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
   // Grabbing our GeoJSON data..
 d3.json(link, function(data) {
     // Creating a GeoJSON layer with the retrieved data
-    L.geoJson(data).addTo(myMap);
+    console.log(data);
+    // L.geoJson(data).addTo(myMap);
   });
 
-*/
+
+
+
+
+var myMap = L.map("mapid", {
+    // center: [45.52, -122.67],
+    center: [34.0522, -118.2437],
+    zoom: 5
+  });
+  
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
 
