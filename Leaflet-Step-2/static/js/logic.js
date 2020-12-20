31f7bbf042aef7d9f62a5cb7a0c7a9cdbd8126d1
LenSin3 <lenpk3@yahoo.co.uk>
2020-12-20 13:09:53 -0800

    rendered map with tectonic plate geodata

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,4 +1,56 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    //layers: [lightmap]
+  });
+  
+// Satelite Layer
+var satelite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+/*
+// Satelite Layer
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/outdoors-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+*/
+
+
+/*
+  // Create the tile layer that will be the background of our map
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  }).addTo(myMap);
+*/
+
 var link = "static/data/PB2002_plates.json";
 d3.json(link, function(data) {
     console.log(data);
+    L.geoJson(data, {
+
+        
+        style: {
+            color: "#FFA500",
+            opacity: 0.6,
+            fillColor: '#333333',
+            fillOpacity: 0
+        }
+
+    }).addTo(myMap);
 })
